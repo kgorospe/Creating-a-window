17a259348bc07158550a01b5874bbe9d55539ab8
James Gorospe <gorospe.james@gmail.com>
2021-07-25 11:47:00 -0500

    Update main.py
    review added comments

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from tkinter import *
+from tkinter import * #Explain what this is - JG
 
 def create_window():
     new_window = Tk()
@@ -7,7 +7,7 @@ def create_window():
     new_window.title('Welcome to my second window')
     label = Label(new_window, text="Hello World").pack()
 
-window = Tk()
+window = Tk() #Explain what this is - JG
 window.title('My First Window')
 Button(window, text="Next Window", command=create_window).pack()
-window.mainloop()
+window.mainloop() #Explain what this is - JG
